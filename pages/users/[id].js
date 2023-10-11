30a21c578ca7165d41a95c26cbc241cf17e78a16
Muhammad Andika <74449013+akrn27@users.noreply.github.com>
2023-10-11 16:37:21 +0700

    Tugas - Hari 8

---
+++ pages/users/[id].js
@@ -0,0 +1,18 @@
+import { Layout } from "@/layout";
+import { useRouter } from "next/router";
+import React from "react";
+
+const UsersByName = () => {
+  const router = useRouter();
+  const { id } = router?.query;
+
+  console.log(id);
+
+  return (
+    <Layout>
+      <p>Users by {id}</p>
+    </Layout>
+  );
+};
+
+export default UsersByName;
